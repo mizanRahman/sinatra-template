dcabdc2ff29aa426ba6b00e7059d6f261d284144
Mizan Rahman <mizan.cse.buet@gmail.com>
2013-08-09 02:03:25 +0600

    modularized

--- application.rb
+++ application.rb
@@ -1,23 +1,30 @@
 require 'rubygems'
 require 'bundler/setup'
-require 'sinatra'
+require 'sinatra/base'
 require File.join(File.dirname(__FILE__), 'environment')
 
-configure do
-  set :views, "#{File.dirname(__FILE__)}/views"
-end
+require 'haml'
 
-error do
-  e = request.env['sinatra.error']
-  Kernel.puts e.backtrace.join("\n")
-  'Application error'
-end
+class MainApp < Sinatra::Base
+  configure do
+	  set :views, "#{File.dirname(__FILE__)}/views"
+	end
 
-helpers do
-  # add your helpers here
-end
+	error do
+	  e = request.env['sinatra.error']
+	  Kernel.puts e.backtrace.join("\n")
+	  'Application error'
+	end
 
-# root page
-get '/' do
-  haml :root
+	helpers do
+	  # add your helpers here
+	end
+
+	# root page
+	get '/' do
+	  haml :root
+	end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
